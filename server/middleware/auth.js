1757c8a5a3c546a84a4701e442495b102e8e040f
RudyRed <willgross94@gmail.com>
2017-11-07 08:27:56 -0800

    Still on 19

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,17 +2,7 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  var session = models.Sessions;
-
-  //if the request has a cookie- look to see if it has a valud session
-  // if (req.cookies && req.cookies.hasOwnProperties('shortlyid')) {
-  //   models.Session.get({hash: req.cookies['shortlyid']})
-  //   .then( (session) => {
-  //     if (session) {
-  //
-  //     }
-  //   })
-  // }
+  let session = models.Sessions;
 
   if (req.headers.cookie === undefined) {
     session.create()
