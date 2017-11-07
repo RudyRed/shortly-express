e1998e041a33e17c8ea62303449bfc9fdb27a103
RudyRed <willgross94@gmail.com>
2017-11-06 17:36:23 -0800

    working on the middleware cookie parser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,22 @@
 const parseCookies = (req, res, next) => {
+
+  console.log(req.headers.cookie,'&&&&&&&&&&&&&&&&&&&&&&&');
+  if (!req.headers.cookie) {
+    next();
+  } else {
+    console.log(req)
+    var cookie = req.headers.cookie.split(/=|; /g);
+    // console.log(req)
+    console.log(req.headers.cookie, 'HHEHRFWEHFHCHEFWHFHF')
+    console.log(cookie, 'SPLIT COOKIE')
+
+    for (var i = 0; i < cookie.length; i += 2) {
+      req.cookies[cookie[i]] = cookie[i + 1];
+    }
+
+    next();
+  }
+
 };
 
 module.exports = parseCookies;
